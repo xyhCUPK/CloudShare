c5111ca1a4399fd92fe241cc7d8251a19a0a1614
xyhao <xyhaomail@163.com>
2023-07-10 12:55:29 +0800

    创建了一个新项目

---
+++ src/main/java/com/CloudShare/CloudShareApplication.java
@@ -0,0 +1,13 @@
+package com.CloudShare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudShareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/CloudShare/CloudShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CloudShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
